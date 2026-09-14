b38eb16cd13755f50be007bea9cc62104a27fbef
Eng Soft Marcelo <engsoftmarcelo@example.com>
2026-09-14 19:57:00 -0300

    first commit

---
+++ src/main/java/com/example/atividade2_login/Atividade2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade2_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2LoginApplication.class, args);
+	}
+
+}
